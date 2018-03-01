5e9a817f7c600743276d012b062db42fa4a2d0dc
hxeecs <xueyuh@gmail.com>
2018-03-01 09:19:02 +0800

    first version

---
+++ src/component/mycomponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="mycomponent">
+    <input v-model="first" placeholder="FIRST NAME">
+    <input v-model="last" placeholder="LAST NAME">
+    <p> Hello {{first}} {{last}}</p>
+
+    <a> by {{ author }} </a>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+        author: "Brian H",
+        first: "",
+        last: ""
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
